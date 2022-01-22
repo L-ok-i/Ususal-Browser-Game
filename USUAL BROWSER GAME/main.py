9eb1b2159ce479938e780ef6d7bf9bf6ff188a98
None <48880563+L-ok-i@users.noreply.github.com>
2022-01-22 13:00:08 +0700

    Add files via upload

--- USUAL BROWSER GAME/main.py
+++ USUAL BROWSER GAME/main.py
@@ -31,7 +31,6 @@ btn_restart = pygame.image.load('img/png/buttons/btn-restart.png').convert_alpha
 clock = pygame.time.Clock()
 FPS = 60  
 
-p = 1
 b = True
 
 x = 120
@@ -49,7 +48,8 @@ class Game:
         pygame.mouse.set_cursor(color)
         pygame.display.set_caption('Мы Русские с нами pygame!')
         pygame.mixer.music.load('music/music_menu.mp3')
-
+        global scene
+        scene = Prev()
         while 1:
             clock.tick(FPS)
             for event in pygame.event.get():
@@ -57,32 +57,32 @@ class Game:
                     pygame.quit()
                     sys.exit()
             
-            if p == 1:
-                Prev(event)
-            elif p == 2:
-                Menu(event)
-            elif p == 3:
-                Training(event)
-                GG()
-            elif p == 4:
-                Pause(event)
+                scene.e(event)
+            scene.draw()
+
+                
 
             pygame.display.update()
 
 class Prev:
-    def __init__(self, event):
-        global b, p
+    def draw(self):
+        global b
         if b:
             b = False
             screen.blit(img_logo, (230, 170))
         
+    def e(self, event):
+        global scene
         if event.type == pygame.MOUSEBUTTONUP:
-            pygame.time.delay(800)
-            p = 2
+            # pygame.time.delay(800)
+            print('01')
+            scene = Menu()
+            
 
 class Menu:
-    def __init__(self, event):
-        global b, p
+    def draw(self):
+        global b
+
         if not b:
             b = True
             screen.blit(bg_menu, (0, 0))
@@ -93,33 +93,56 @@ class Menu:
 
         if 120 < mouse[0] < 357 and 180 < mouse[1] < 217:
             screen.blit(btn_1_act, (120, 180))
-             # if event.type == pygame.MOUSEBUTTONUP: # кампания
                 
         else:
             screen.blit(btn_1, (120, 180))
         if 120 < mouse[0] < 357 and 250 < mouse[1] < 287:
             screen.blit(btn_2_act, (120, 250))
-            if event.type == pygame.MOUSEBUTTONUP:
-                pygame.time.delay(200)
-                p = 3
         else:
             screen.blit(btn_2, (120, 250))
         if 120 < mouse[0] < 357 and 320 < mouse[1] < 357:
             screen.blit(btn_3_act, (120, 320))
-            if event.type == pygame.MOUSEBUTTONUP:
-                pygame.time.delay(200)
-                pygame.quit()
-                sys.exit()
         else:
             screen.blit(btn_3, (120, 320))
-  
+
+    def e(self, event):
+        global scene
+        mouse = pygame.mouse.get_pos()
+
+        if 120 < mouse[0] < 357 and 180 < mouse[1] < 217:
+            pass
+            # if event.type == pygame.MOUSEBUTTONUP: # кампания
+            #
+        if 120 < mouse[0] < 357 and 250 < mouse[1] < 287:
+            if event.type == pygame.MOUSEBUTTONUP:
+                     pygame.time.delay(200)
+                     scene = Training()
+        if 120 < mouse[0] < 357 and 320 < mouse[1] < 357:
+            if event.type == pygame.MOUSEBUTTONUP:
+                    pygame.time.delay(200)
+                    pygame.quit()
+                    sys.exit()
+
+
+
+
+
+
 class Training:
-    def __init__(self, event):
+    def __init__(self):
+        gg = GG()
+        print('02')
+
+    def draw(self):
         global p
         screen.blit(bg_training, (0, 0))
         screen.blit(btn_pause, (5, 5))
         screen.blit(btn_restart, (45, 5))
-      
+        gg = GG()
+
+        
+    def e(self, event):
+        global scene
         mouse = pygame.mouse.get_pos()
         # click = pygame.mouse.get_pressed()
         keys = pygame.key.get_pressed()
@@ -127,48 +150,55 @@ class Training:
         if (event.type == pygame.MOUSEBUTTONUP and 5 < mouse[0] < 37 and 5 < mouse[1] < 37) or keys[pygame.K_ESCAPE]:  # PAUSE
             # ПРОРИСОВКА ОСТАНАВЛИВАЕТСЯ!!!ДОЛЖНА ОСТАНАВЛИВАТЬСЯ, но пока сделаю так.
             pygame.time.delay(100)
-            p = 4
+            scene = Pause()
         if (event.type == pygame.MOUSEBUTTONUP and 45 < mouse[0] < 77 and 5 < mouse[1] < 37) or keys[pygame.K_F5]:  # RESTART
             pygame.time.delay(100)
             # .....................
             # restart прорисовки
             print('ns lt,bk') #  наврятли нужен
         
+        
 class Pause:
-     def __init__(self, event):
+    def draw(self):
         global b, p
         if b:
             b = False
             screen.blit(bg_menu, (0, 0))
             # screen.blit(btns_menu, (38, 99))
-
         mouse = pygame.mouse.get_pos()
         # click = pygame.mouse.get_pressed()
 
         if 120 < mouse[0] < 357 and 180 < mouse[1] < 217:
             screen.blit(btn_4_act, (120, 180))
-            if event.type == pygame.MOUSEBUTTONUP:
-                b = True
-                p = 3
-                
         else:
             screen.blit(btn_4, (120, 180))
         if 120 < mouse[0] < 357 and 250 < mouse[1] < 287:
             screen.blit(btn_2_act, (120, 250))
-            if event.type == pygame.MOUSEBUTTONUP:
-                pygame.time.delay(200)
-                b = True
-                p = 3
         else:
             screen.blit(btn_2, (120, 250))
         if 120 < mouse[0] < 357 and 320 < mouse[1] < 357:
             screen.blit(btn_3_act, (120, 320))
+        else:
+            screen.blit(btn_3, (120, 320))
+
+    def e(self, event):
+        global b, scene
+        mouse = pygame.mouse.get_pos()
+
+        if 120 < mouse[0] < 357 and 180 < mouse[1] < 217:
+            if event.type == pygame.MOUSEBUTTONUP:
+                b = True
+                scene = Training()
+        if 120 < mouse[0] < 357 and 250 < mouse[1] < 287:
+            if event.type == pygame.MOUSEBUTTONUP:
+                b = True
+                pygame.time.delay(200)
+                scene = Training()
+        if 120 < mouse[0] < 357 and 320 < mouse[1] < 357:
             if event.type == pygame.MOUSEBUTTONUP:
                 pygame.time.delay(200)
                 pygame.quit()
                 sys.exit()
-        else:
-            screen.blit(btn_3, (120, 320))
 
 class GG:
     def __init__(self):
@@ -185,9 +215,6 @@ class GG:
             x -= speed
         if keys[pygame.K_UP]:
             y -= 4
-        
-
-
 
         if is_player_heading_right:
             screen.blit(gg_heading_right, (x, y)) 
